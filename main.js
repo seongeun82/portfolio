49701d5424cb8989b97fdfd0b26816b226e15d66
seongeun82 <67671890+seongeun82@users.noreply.github.com>
2021-03-15 06:41:24 +0900

    js navbar_menu click scroll

--- main.js
+++ main.js
@@ -3,13 +3,29 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-
+// Make navbar transparent whet it is on the top 
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(`navbar_height: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
-})
+});
+
+// Handle scrolling when tapping on the navbar menu 
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  console.log(event.target.dataset.link);
+  const target = event.target;
+  const link = target.dataset.link;
+  console.log(link)
+  if (link == null) {
+    return;
+  };
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({
+    behavior: 'smooth'
+  });
+
+});
