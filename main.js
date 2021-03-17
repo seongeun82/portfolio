1f60d16b686c4f336da2d418443d56d0e5d582d9
seongeun82 <67671890+seongeun82@users.noreply.github.com>
2021-03-18 07:18:08 +0900

    js arrow up

--- main.js
+++ main.js
@@ -15,24 +15,49 @@ document.addEventListener('scroll', () => {
 // Handle scrolling when tapping on the navbar menu 
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-  console.log(event.target.dataset.link);
+  // console.log(event.target.dataset.link);
   const target = event.target;
   const link = target.dataset.link;
-  console.log(link)
+  // console.log(link)
   if (link == null) {
     return;
   };
-
   scrollIntoView(link);
-
-
 });
 
 
 const contactMeIcon = document.querySelector('.home__contact');
 contactMeIcon.addEventListener('click', () => {
   scrollIntoView('#contact')
+});
+
+
+// home은 내려올 수록 점점 투명해짐 
+const home = document.querySelector('#home .section__container');
+const home_height = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  const newHomeOpacity = 1 - (window.scrollY / home_height);
+  home.style.opacity = newHomeOpacity;
+});
+
+// Show "arrow__up" button when scrolling down 
+const arrowup = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > home_height / 2) {
+    arrowup.classList.add('visible');
+  } else {
+    arrowup.classList.remove('visible')
+  }
+});
+
+arrowup.addEventListener('click', () => {
+  scrollIntoView('#home')
 })
+
+
+
+
 
 function scrollIntoView(selector) {
   scrollTo = document.querySelector(selector);
